e455f7388c93378721df30f1e4e842db548582d5
Karl <karlanuetzel@gmail.com>
2017-10-28 16:31:58 -0500

    anotha one

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,14 @@ var http = require('http');
 var https = require('https');
 var querystring = require('querystring');
 var router = express.Router();
+let apiKey = 'frXv9fqR24i3FbjXKFbnULOecCoDGMC78ZHvHjmC';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
 
-router.post('/suggestedSearch', function(req, response){
-
-    let apiKey = 'frXv9fqR24i3FbjXKFbnULOecCoDGMC78ZHvHjmC';
-    let url = 'https://api.nal.usda.gov/ndb/search/?';
-    let maxRows = 10;
-    let searchKey = req.body.key;
-    let properties = {format: 'json', q:searchKey, max:maxRows, api_key: apiKey};
+function sendData(url, properties, response){
     let query =  querystring.stringify(properties);
     let path = url + query;
     https.get(path, res => {
@@ -28,5 +23,20 @@ router.post('/suggestedSearch', function(req, response){
             response.status(200).send(body);
         });
     });
+}
+
+router.post('/suggestedSearch', function(req, response){
+    let url = 'https://api.nal.usda.gov/ndb/search/?';
+    let maxRows = 10;
+    let searchKey = req.body.key;
+    let properties = {format: 'json', q:searchKey, max:maxRows, api_key: apiKey};
+    sendData(url, properties, response);
+});
+
+router.post('/nutrition', function(req, response){
+    let url = 'https://api.nal.usda.gov/ndb/V2/reports?';
+    let searchKey = req.body.key;
+    let properties = {format: 'json', ndbno:searchKey, api_key: apiKey};
+    sendData(url, properties, response);
 });
 module.exports = router;
